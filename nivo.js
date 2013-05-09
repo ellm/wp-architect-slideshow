bb929b45dd9427b39487f6e0e2ab9cd80dddfd26
Matthew Ell <ell.matthew@gmail.com>
2013-05-09 16:25:46 -0400

    Update to Lables
    • Commented out resources
    
    Signed-off-by: Matthew Ell <ell.matthew@gmail.com>

--- nivo.js
+++ nivo.js
@@ -8,7 +8,7 @@ $(window).load(function() {
         animSpeed: 500, // Slide transition speed
         pauseTime: 3000, // How long each slide will show
         startSlide: 0, // Set starting Slide (0 index)
-        directionNav: false, // Next & Prev navigation
+        directionNav: true, // Next & Prev navigation
         controlNav: true, // 1,2,3... navigation
         controlNavThumbs: false, // Use thumbnails for Control Nav
         pauseOnHover: true, // Stop animation while hovering
--- slideshow.php
+++ slideshow.php
@@ -1,7 +1,7 @@
 <?php  
 /* 
-    Plugin Name: wp-Architect Slideshow Plugin 
-    Description: Simple Nivo Slideshow for WP-Architect Theme
+    Plugin Name: Homepage Slideshow  
+    Description: Homepage Slideshow for WP-Architect Theme
     Author: Matthew Ell 
     Version: 1.0 
 */  
@@ -12,17 +12,86 @@ function wp_arch_ss_init() {
     
     $args = array(  
         'public' => true,  
-        'label' => 'Nivo Images',  
+        'labels' => array(
+            'name' => _x('Slides', 'post type general name'),
+            'singular_name' => _x('Slide', 'post type singular name'),
+            'add_new' => _x('Add New', 'event'),
+            'add_new_item' => __('Add New Slide'),
+            'edit_item' => __('Edit Slide'),
+            'new_item' => __('New Slide'),
+            'view_item' => __('View Slide'),
+            'search_items' => __('Search Slide'),
+            'not_found' =>  __('No events found'),
+            'not_found_in_trash' => __('No events found in Trash'), 
+            'parent_item_colon' => ''
+        ),
+        'description'  => 'Nivo Homepage Slideshow',
+        'exclude_from_search' => true,
         'supports' => array(  
             'title',  
-            'thumbnail'  
-        ) 
+            'thumbnail'
+        )
     ); 
-    register_post_type('np_images', $args);  
-
+    // http://codex.wordpress.org/Function_Reference/register_post_type
+    register_post_type('np_images', $args);
 }  
 
 add_action('init', 'wp_arch_ss_init');
+
+
+// Creates Admin Options sub-page
+//http://codex.wordpress.org/Function_Reference/add_submenu_page
+
+// add_action('admin_menu', 'register_my_custom_submenu_page');
+
+// function register_my_custom_submenu_page() {
+//     add_submenu_page( 'edit.php?post_type=np_images', 'Settings', 'Settings', 'manage_options', 'my-custom-submenu-page', 'my_custom_submenu_page_callback' ); 
+// }
+
+// function my_custom_submenu_page_callback() {
+//     echo '<div class="wrap">';
+//     echo '<h2>Slideshow Settings</h2>';
+//     echo '</div>';
+
+// }
+
+// Creates Tables for Settings Pages
+// http://codex.wordpress.org/Creating_Tables_with_Plugins
+
+//     global $archss_db_version;
+//     $archss_db_version = "1.0";
+
+//     function archss_install() {
+//         global $wpdb;
+//         global $archss_db_version;
+
+//         $table_name = $wpdb->prefix . "archss";
+      
+//         $sql = "CREATE TABLE $table_name (
+//             id mediumint(9) NOT NULL AUTO_INCREMENT,
+//             time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+//             name tinytext NOT NULL,
+//             text text NOT NULL,
+//             url VARCHAR(55) DEFAULT '' NOT NULL,
+//             UNIQUE KEY id (id)
+//         );";
+
+//         require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+//         dbDelta( $sql );
+ 
+//     add_option( "archss_db_version", $archss_db_version );
+// }
+
+//     function archss_install_data() {
+//         global $wpdb;
+//         $welcome_name = "Mr. WordPress";
+//         $welcome_text = "Congratulations, you just completed the installation!";
+
+//         $rows_affected = $wpdb->insert( $table_name, array( 'time' => current_time('mysql'), 'name' => $welcome_name, 'text' => $welcome_text ) );
+//     }
+
+// register_activation_hook( __FILE__, 'archss_install' );
+// register_activation_hook( __FILE__, 'archss_install_data' );
 
 // Enqueue Styles and Scripts 
 // http://codex.wordpress.org/Determining_Plugin_and_Content_Directories
@@ -53,24 +122,30 @@ add_image_size('np_function', 1024, 399, true);
 add_theme_support( 'post-thumbnails' ); 
 
 // // // Create Slideshow
-function np_function($type='np_function') {  
+function np_function( $atts) { 
+
+    // extract( shortcode_atts ( array(
+    //     'width' => '',
+    //     'height' => '',
+    //     ), $atts ) ); 
     
     $args = array(  
         'post_type' => 'np_images',  
-        'posts_per_page' => 5  
+        'posts_per_page' => 5 
     );  
 
     $result = '<section id="slideshow" class="theme-default">';  
-    $result .= '<div class="nivoSlider inner">';  
+    $result .= '<div class="nivoSlider inner">';
   
     // The Query
+    //http://codex.wordpress.org/Function_Reference/WP_Query
     $loop = new WP_Query($args);  
 
     // The Loop
     while ($loop->have_posts()) {  
         $loop->the_post();  
         $the_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $type);  
-        $result .='<img title="'.get_the_title().'" src="' . $the_url[0] . '" alt=""/>'; 
+        $result .='<img title="'.get_the_title().'" src="' . $the_url[0] . '" alt=""/>';
     }  
 
 
